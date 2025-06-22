2f1a177853da025815588624dcafaedd04a99461
SLIM BEL ABRIA <belabria26@gmail.com>
2025-06-22 20:32:43 +0100

    added theme file

--- menasyp/lib/core/theme.dart
+++ menasyp/lib/core/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 
-const Color  backgroundColor= const Color(0xFF101010);
+const Color  backgroundColor=  Color(0xFF101010);
 const Color secondaryColor = Color(0xffFF2057);
 const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
