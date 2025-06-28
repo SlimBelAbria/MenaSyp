45a1f456babfc73df32189ec125f3c40f29af069
SLIM BEL ABRIA <belabria26@gmail.com>
2025-06-28 15:39:21 +0100

    UI and Functionalities enhancements

--- menasyp/lib/core/theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-const Color  backgroundColor=  Color(0xFF101010);
-const Color secondaryColor = Color(0xffFF2057);
-const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
